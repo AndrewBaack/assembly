d91b1d80dd3680c81827201d66b49725ffe9870f
AndrewBaack <anba6639@colorado.edu>
2026-02-06 17:11:35 -0700

    Update sum-n.s

--- sum-n.s
+++ sum-n.s
@@ -1,12 +1,20 @@
 .text
 _start:
     movia   r4, N
-    ldw     r4, 0(r4)
-    movi    r2, 0
+    ldw     r4, 0(r4) 
+    movi    r2, 0 
 
-    # Your code here
+    movi    r3, 1
 
+loop:
+    bgt     r3, r4, done
+    add     r2, r2, r3
+    addi    r3, r3, 1
+    br      loop
+
+done:
     break
 
 .data
 N:  .word 9
+
