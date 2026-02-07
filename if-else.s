bc4e6c0636707fc03793ab7917d88ef707179af7
AndrewBaack <anba6639@colorado.edu>
2026-02-06 17:15:35 -0700

    Update if-else.s

--- if-else.s
+++ if-else.s
@@ -1,3 +1,19 @@
 .text
 _start:
     
+    mov     r2, r4
+
+    bgt     r5, r2, r5_is_bigger
+    br      check_r6
+
+r5_is_bigger:
+    mov     r2, r5
+
+check_r6:
+    bgt     r6, r2, r6_is_bigger
+    br      done
+
+r6_is_bigger:
+    mov     r2, r6
+
+done:
